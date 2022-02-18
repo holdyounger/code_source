bf27aef408099e127a1e33dda7e411c55882887d
holdyounger <holdyounger@163.com>
2022-02-18 15:05:34 +0800

    :rocket: 回调函数完善

--- Demo_C++/开发测试.vs2017/CallBackFunc/CallBackFunc.cpp
+++ Demo_C++/开发测试.vs2017/CallBackFunc/CallBackFunc.cpp
@@ -2,25 +2,26 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 
-int toZeroDown(int n, void *contex) {
+string toZeroDown(int n, void *contex) {
 	cout  << "toZeroDown:" << -*(int*)(contex) << endl;
-	return 0;
+	return "toZeroDown";
 }
 
-int toZeroUp(int n, void *contex) {
+string toZeroUp(int n, void *contex) {
 	cout << "toZeroUp:" << *(int*)(contex) << endl;
-	return 0;
+	return "toZeroUp";
 }
 
-typedef int(*CallBackFunc)(int n, void *contex);
+typedef string(*CallBackFunc)(int n, void *contex);
 
 void registNumCallBack(CallBackFunc callback, void *contex)
 {
 	int n = 3;
-	cout << callback(n, contex);
+	cout << "The Option of 'callback()' is:" << callback(n, contex) << endl;
 }
 
 int main()
@@ -37,14 +38,3 @@ int main()
 	}
 	
 }
-
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
